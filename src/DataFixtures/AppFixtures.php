744256af2ad44776ebaa07e37897f803411d8366
Esteban Ristich <esteban.ristich@protonmail.com>
2023-05-24 10:53:46 +0200

    feat(fixture): add method to create random rooms for users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\GameRoom;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -27,6 +28,7 @@ class AppFixtures extends Fixture
         $this->manager = $manager;
         $this->makeAdmin();
         $this->makeUser(15);
+        $this->makeRoom(20, 40);
     }
 
     private function makeAdmin(): void
@@ -53,6 +55,24 @@ class AppFixtures extends Fixture
                 ->setRoles(['ROLE_USER'])
             ;
             $this->manager->persist($user);
+        }
+        $this->manager->flush();
+    }
+
+    private function makeRoom(int $rndMin, int $rndMax): void
+    {
+        $numRoom = random_int($rndMin, $rndMax);
+        $allUser = $this->manager->getRepository(User::class)->findAll();
+        for ($i = 0; $i < $numRoom; $i++) {
+            $currentOwner = $this->faker->randomElement($allUser);
+            $gameRoom = new GameRoom();
+            $gameRoom
+                ->setName('Room #'.$i + 1)
+                ->setOwner($currentOwner)
+                ->setState('FINISHED')
+                ->addParticipant($currentOwner)
+            ;
+            $this->manager->persist($gameRoom);
         }
         $this->manager->flush();
     }
