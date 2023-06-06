f4ceb56d67ab0179cebac337456e8c1fbc811f1b
Esteban Ristich <esteban.ristich@protonmail.com>
2023-06-07 01:11:43 +0200

    feat(controller): create pledge controller

---
+++ src/Controller/PledgeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/v1/pledge')]
+#[OA\Tag(name: 'Pledge')]
+#[OA\Response(
+    response: 400,
+    description: 'Bad request'
+)]
+#[OA\Response(
+    response: 401,
+    description: 'Unauthorized'
+)]
+class PledgeController extends AbstractController
+{
+}
