b47901ed83b3cfd4749a08b9438a4d52608250a6
Esteban Ristich <esteban.ristich@protonmail.com>
2023-06-08 14:39:22 +0200

    feat(package): add nelmio/cors-bundle dependency
    * Authorize cross origin sites in default config

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
